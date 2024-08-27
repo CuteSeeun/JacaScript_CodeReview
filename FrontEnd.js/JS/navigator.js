e298ce01873a479fad281301230e10b12c31efe6
최세은 <seeun2190@gmail.com>
2024-08-27 11:36:56 +0900

    SMS file upload

--- FrontEnd.js/JS/navigator.js
+++
@@ -1,7 +0,0 @@
-console.log(navigator.userActivation);
-console.dir(navigator);
-
-const string = new String("바보");
-console.log(string);
-console.dir(string);
-
--- Syntax/node-basic/StandardModule/os.js
+++
@@ -1,4 +0,0 @@
-//require가 필요하다
-const os = require("os");
-
-console.log(os.cpus().length);
