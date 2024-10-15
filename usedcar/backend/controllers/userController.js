e35f800d267e5ed224fb464a634e50f0d6077fe2
bigbro5232 <th0102cr@gmail.com>
2024-10-15 17:07:41 +0900

    [회원가입,로그인] 회원가입 구현
    로그인 페이지 폼만 완성 회원가입 기능은 구현완료

--- usedcar/backend/controllers/userController.js
+++ usedcar/backend/controllers/userController.js
@@ -1,7 +1,14 @@
+// userController.js
 const pool = require('../dbpools');
 
 const saveUser = async (req, res) => {
     const { name, userid, passwd, tel, email } = req.body;
+
+    // 빈 문자열 확인
+    if (!name || !userid || !passwd || !tel || !email) {
+        return res.status(400).json({ message: 'Feild Error' });
+    }
+
     try {
         const [result] = await pool.query('INSERT INTO user (name, userid, passwd, tel, email) VALUES (?, ?, ?, ?, ?)', [name, userid, passwd, tel, email]);
         res.json({ id: result.insertId, name, userid, passwd, tel, email });
@@ -11,4 +18,16 @@ const saveUser = async (req, res) => {
     }
 };
 
-module.exports = saveUser;
+const verifyEmail = async (req, res) => {
+    const { email } = req.body;
+    try {
+        const [rows] = await pool.query('SELECT * FROM user WHERE email = ?', [email]);
+        const emailExists = rows.length > 0;
+        res.json({ exists: emailExists });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error');
+    }
+};
+
+module.exports = { saveUser, verifyEmail };
