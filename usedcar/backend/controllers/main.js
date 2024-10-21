b600f826cbfe9800b57852bcca912c2738ca1124
lakelover0611 <jyknowu@gmail.com>
2024-10-21 10:19:43 +0900

    [carlistoutput] carlist 최신순 정렬 기준 변경
    before: 차 연식 기준으로 최신순 정렬
    after: 게시물 등록 기준으로 최신순 정렬

--- usedcar/backend/controllers/main.js
+++ usedcar/backend/controllers/main.js
@@ -3,28 +3,29 @@
 const pool = require("../config/dbpools");
 
 const carinfo = async (req, res) => {
-  const { user_uno } = req.query;  // 유저 번호를 쿼리로 받기
+  const { user_uno } = req.query; // 유저 번호를 쿼리로 받기
 
-
-    const sql = ` SELECT 
+  const sql = ` SELECT 
     car.*, 
     IFNULL(favorite.favorite, 0) AS favorite,
     user.name AS seller_name, 
     user.tel AS seller_tel, 
     user.email AS seller_email,
-    board.sale  -- sale 값을 추가
+    board.date,
+    board.sale
     FROM car LEFT JOIN 
     favorite ON car.cNo = favorite.car_cNo AND favorite.user_uNo = ?
     JOIN user ON car.user_uno = user.uNo
-    JOIN board ON car.cNo = board.car_cNo  -- board 테이블과 join
-    WHERE board.sale = 0;  -- sale 값 필터링`;
+    JOIN board ON car.cNo = board.car_cNo 
+    WHERE board.sale = 0
+    ORDER BY board.date DESC `;
   try {
-    const [data] = await pool.query(sql,[user_uno]); // user_uno 전달
+    const [data] = await pool.query(sql, [user_uno]); // user_uno 전달
     res.status(200).json(data);
   } catch (error) {
     console.error("Error fetching car data:", error);
     res.status(500).send(error.message);
   }
-}
+};
 
 module.exports = carinfo;
