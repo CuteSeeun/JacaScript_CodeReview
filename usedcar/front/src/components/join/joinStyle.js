6e6bc165805f9020e800e932dac993e539787644
lakelover0611 <jyknowu@gmail.com>
2024-10-21 16:40:53 +0900

    [design] 색깔 수정
    header,footer, 버튼 색깔 수정, 로고 변경

--- usedcar/front/src/components/join/joinStyle.js
+++ usedcar/front/src/components/join/joinStyle.js
@@ -1,66 +1,68 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const JoinStyle = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh; /* 화면의 높이를 100%로 설정 */
-    margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* 화면의 높이를 100%로 설정 */
+  margin: 0;
 
-    .signup-container {
-        width: 100%;
-        max-width: 400px;
-        padding: 20px;
-        border-radius: 10px;
-        background-color: white;
-        box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
-        margin: 15px 0;
-    }
+  .signup-container {
+    width: 100%;
+    max-width: 400px;
+    padding: 20px;
+    border-radius: 10px;
+    background-color: white;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+    margin: 15px 0;
+  }
 
-    h1 {
-        font-size: 1.8rem;
-        font-weight: 600;
-        text-align: center;
-        margin-bottom: 30px;
-    }
+  h1 {
+    font-size: 1.8rem;
+    font-weight: 600;
+    text-align: center;
+    margin-bottom: 30px;
+  }
 
-    .form-control {
-        border-radius: 5px;
-        height: 45px;
-    }
+  .form-control {
+    border-radius: 5px;
+    height: 45px;
+  }
 
-    .signup-btn {
-        background-color: #e84545;
-        border-radius: 5px;
-        height: 50px;
-        font-size: 1.2rem;
-    }
+  .signup-btn {
+    background-color: #007bff;
+    border: none;
+    color: #fff;
+    border-radius: 5px;
+    height: 50px;
+    font-size: 1.2rem;
+  }
 
-    .signup-btn:hover {
-        background-color: #d73939;
-    }
+  .signup-btn:hover {
+    background-color: #0056b3;
+  }
 
-    .form-label {
-        font-size: 0.9rem;
-        color: #6c757d;
-        font-weight: 600;
-    }
+  .form-label {
+    font-size: 0.9rem;
+    color: #6c757d;
+    font-weight: 600;
+  }
 
-    .required-label {
-        color: red;
-    }
+  .required-label {
+    color: red;
+  }
 
-    .form-text {
-        font-size: 0.8rem;
-        color: #6c757d;
-    }
+  .form-text {
+    font-size: 0.8rem;
+    color: #6c757d;
+  }
 
-    .back-btn {
-        color: #6c757d;
-        font-size: 1.5rem;
-    }
+  .back-btn {
+    color: #6c757d;
+    font-size: 1.5rem;
+  }
 
-    .back-btn:hover {
-        color: #000000;
-    }
+  .back-btn:hover {
+    color: #000000;
+  }
 `;
--- usedcar/front/src/components/mainhj/CarListMain.jsx
+++ usedcar/front/src/components/mainhj/CarListMain.jsx
@@ -1,104 +1,138 @@
-import React, { useEffect, useState } from 'react';
-import CarListBanner from './CarListBanner';
-import CarListOutput from './CarListOutput';
-import CarListTop from './CarListTop';
-import { CarListMainWrap, ContentWrap } from './carListStyle';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import CarListBanner from "./CarListBanner";
+import CarListOutput from "./CarListOutput";
+import CarListTop from "./CarListTop";
+import { CarListMainWrap, ContentWrap } from "./carListStyle";
+import axios from "axios";
 
 const CarListMain = () => {
-    const [carList, setCarList] = useState([]);
-    const [search, setSearch] = useState('');
-    const [filteredCars, setFilteredCars] = useState([]);
+  const [carList, setCarList] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filteredCars, setFilteredCars] = useState([]);
 
-    const [input , setInput] = useState('');
+  const [input, setInput] = useState("");
 
-    const [filters, setFilters] = useState({
-        brand: '',
-        year: '',
-        mileage: '',
-        fueltype: '',
-        price: '',
-        color: '',
+  const [filters, setFilters] = useState({
+    brand: "",
+    year: "",
+    mileage: "",
+    fueltype: "",
+    price: "",
+    color: "",
+  });
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    const carListData = async () => {
+      const user_uno = localStorage.getItem("uNo"); // 유저 번호 가져오기
+      try {
+        const response = await axios.get(
+          `http://localhost:3333/car?user_uno=${user_uno}`
+        );
+        setCarList(response.data);
+        console.log("이펙트 :", response.data);
+
+        setFilteredCars(response.data);
+      } catch (error) {
+        console.error("carList error", error);
+      }
+    };
+    carListData();
+  }, []);
+
+  useEffect(() => {
+    const filtered = filterCars(carList, filters, search);
+    setFilteredCars(filtered);
+    setCurrentPage(1);
+  }, [filters, search, carList]);
+
+  const filterCars = (cars, filters, search) => {
+    return cars.filter((car) => {
+      const isBrandMatch = !filters.brand || car.brand === filters.brand;
+      const isMileageMatch =
+        !filters.mileage || filterMileage(car.mileage, filters.mileage);
+      const isYearMatch =
+        !filters.year || car.year === parseInt(filters.year, 10);
+      const isPriceMatch =
+        !filters.price || filterPrice(car.price, filters.price);
+      const isSearchMatch =
+        !search || car.name.toLowerCase().includes(search.toLowerCase());
+      const isFuelTypeMatch =
+        !filters.fueltype || car.fueltype === filters.fueltype;
+      const isColorMatch = !filters.color || car.color === filters.color;
+
+      return (
+        isBrandMatch &&
+        isMileageMatch &&
+        isYearMatch &&
+        isPriceMatch &&
+        isSearchMatch &&
+        isFuelTypeMatch &&
+        isColorMatch
+      );
     });
+  };
 
-    const [currentPage , setCurrentPage] = useState(1);
-
-    useEffect(() => {
-        const carListData = async () => {
-            const user_uno = localStorage.getItem('uNo');  // 유저 번호 가져오기
-            try {
-                const response = await axios.get(`http://localhost:3333/car?user_uno=${user_uno}`);
-                setCarList(response.data);
-                console.log('이펙트 :',response.data);
-                
-                setFilteredCars(response.data);
-            } catch (error) {
-                console.error('carList error', error);
-            }
-        };
-        carListData();
-    }, []);
-
-    useEffect(() => {
-        const filtered = filterCars(carList, filters, search);
-        setFilteredCars(filtered);
-        setCurrentPage(1);
-    }, [filters, search, carList]);
-
-    const filterCars = (cars, filters, search) => {
-        return cars.filter(car => {
-            const isBrandMatch = !filters.brand || car.brand === filters.brand;
-            const isMileageMatch = !filters.mileage || filterMileage(car.mileage, filters.mileage);
-            const isYearMatch = !filters.year || car.year === parseInt(filters.year, 10);
-            const isPriceMatch = !filters.price || filterPrice(car.price, filters.price);
-            const isSearchMatch = !search || car.name.toLowerCase().includes(search.toLowerCase());
-            const isFuelTypeMatch = !filters.fueltype || car.fueltype === filters.fueltype;
-            const isColorMatch = !filters.color || car.color === filters.color;
-
-            return isBrandMatch && isMileageMatch && isYearMatch && isPriceMatch && isSearchMatch && isFuelTypeMatch && isColorMatch;
-        });
-    };
-
-    const filterMileage = (carMileage, selectedMileageRange) => {
-        const [minMileage, maxMileage] = selectedMileageRange.split('-').map(v => (v === '+' ? Number.MAX_SAFE_INTEGER : +v));
-        return carMileage >= minMileage && carMileage <= (maxMileage || Number.MAX_SAFE_INTEGER);
-    };
-
-    const filterPrice = (carPrice, selectedPriceRange) => {
-        const priceLimits = {
-            '2000-5000': [2000, 5000],
-            '5000-7000': [5000, 7000],
-            '7000-10000': [7000, 10000],
-            '10000+': [10000, Number.MAX_SAFE_INTEGER]
-        };
-        
-        const [minPrice, maxPrice] = priceLimits[selectedPriceRange] || [0, Number.MAX_SAFE_INTEGER];
-        return carPrice >= minPrice && carPrice <= maxPrice;
-    };
-
-    const searchValue = value =>{
-        setSearch(value);
-
-        const matchedCars = carList.filter(car => car.name.toLowerCase().includes(value.toLowerCase()));
-        
-        if(matchedCars.length > 0){
-            const carBrand = matchedCars[0].brand;
-            setFilters(prevCar =>({
-                ...prevCar,
-                brand:carBrand,
-            }))
-        }
-    }
-
+  const filterMileage = (carMileage, selectedMileageRange) => {
+    const [minMileage, maxMileage] = selectedMileageRange
+      .split("-")
+      .map((v) => (v === "+" ? Number.MAX_SAFE_INTEGER : +v));
     return (
-        <CarListMainWrap>
-            <CarListTop setSearch={searchValue} input={input} setInput={setInput} />
-            <ContentWrap>
-                <CarListBanner filters={filters} setFilters={setFilters} setSearch={setSearch} setInput={setInput} />
-                <CarListOutput carList={filteredCars} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-            </ContentWrap>
-        </CarListMainWrap>
+      carMileage >= minMileage &&
+      carMileage <= (maxMileage || Number.MAX_SAFE_INTEGER)
     );
+  };
+
+  const filterPrice = (carPrice, selectedPriceRange) => {
+    const priceLimits = {
+      "2000-5000": [2000, 5000],
+      "5000-7000": [5000, 7000],
+      "7000-10000": [7000, 10000],
+      "10000+": [10000, Number.MAX_SAFE_INTEGER],
+    };
+
+    const [minPrice, maxPrice] = priceLimits[selectedPriceRange] || [
+      0,
+      Number.MAX_SAFE_INTEGER,
+    ];
+    return carPrice >= minPrice && carPrice <= maxPrice;
+  };
+
+  const searchValue = (value) => {
+    setSearch(value);
+
+    const matchedCars = carList.filter((car) =>
+      car.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if (matchedCars.length > 0) {
+      const carBrand = matchedCars.brand;
+      setFilters((prevCar) => ({
+        ...prevCar,
+        brand: carBrand,
+      }));
+    }
+  };
+
+  return (
+    <CarListMainWrap>
+      <CarListTop setSearch={searchValue} input={input} setInput={setInput} />
+      <ContentWrap>
+        <CarListBanner
+          filters={filters}
+          setFilters={setFilters}
+          setSearch={setSearch}
+          setInput={setInput}
+        />
+        <CarListOutput
+          carList={filteredCars}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
+      </ContentWrap>
+    </CarListMainWrap>
+  );
 };
 
 export default CarListMain;
