b0d83d03de42d119e22209c420f753a9bb67239d
bigbro5232 <th0102cr@gmail.com>
2024-10-21 17:22:39 +0900

    [비밀번호변경] 수정

--- usedcar/front/src/components/login/findPw.js
+++ usedcar/front/src/components/login/findPw.js
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { FindPwStyle } from "./findPwStyle";
 import axios from 'axios';
 import Modal from './Modal';
+import bcrypt from 'bcryptjs';
 
 function FindPw() {
     const [formData, setFormData] = useState({
@@ -40,21 +41,31 @@ function FindPw() {
         }));
     };
 
+
+
     const changePasswd = async (e) => {
         e.preventDefault();
+
         if (!formData.newpasswd || formData.newpasswd.trim() === '') {
             alert('비밀번호를 입력해 주세요.');
             return;
         }
-        if (formData.newpasswd === passWd) {
-            alert('새 비밀번호는 기존 비밀번호와 다르게 설정해야 합니다.');
-            return;
-        }
+
         if (formData.newpasswd !== formData.checkpasswd) {
             alert('비밀번호가 일치하지 않습니다');
             return;
         }
+
         try {
+            const hashednewpw = await bcrypt.hash(formData.newpasswd, 10);
+            console.log(`${hashednewpw}`);
+
+            const isMatch = await bcrypt.compare(passWd, hashednewpw);
+            if (isMatch) {
+                alert('새 비밀번호는 기존 비밀번호와 다르게 설정해야 합니다.');
+                return;
+            }
+
             await axios.post('http://localhost:3333/login/changePw', formData);
             alert('비밀번호가 변경되었습니다');
             setShowModal(false);
@@ -64,6 +75,7 @@ function FindPw() {
             alert('비밀번호 변경에 실패했습니다');
         }
     };
+
 
 
     return (
@@ -101,8 +113,8 @@ function FindPw() {
                         placeholder="이메일 입력"
                     />
                 </div>
-                <button className="btn btn-danger w-100 login-btn mb-3" id="searchBtn" 
-                onClick={handleSubmit}>
+                <button className="btn btn-danger w-100 login-btn mb-3" id="searchBtn"
+                    onClick={handleSubmit}>
                     비밀번호 찾기
                 </button>
             </div>
@@ -129,8 +141,8 @@ function FindPw() {
                             placeholder="비밀번호 확인"
                         />
                     </div>
-                    <button className="btn btn-danger w-100 login-btn mb-3" id="searchBtn" 
-                    onClick={changePasswd}>
+                    <button className="btn btn-danger w-100 login-btn mb-3" id="searchBtn"
+                        onClick={changePasswd}>
                         비밀번호 변경
                     </button>
                 </div>
